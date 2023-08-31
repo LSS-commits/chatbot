149213b4cd0813007f54ca714e2773fcc3d26044
LSS <lss@jaimal.com>
2023-08-31 23:55:01 +0200

    ajout erreur clé

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ import openai
 # pour utiliser les variables d'environnement
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
 # pour ouvrir le navigateur
 import webbrowser
 from threading import Timer
 
+# charger les variables d'environnement
+load_dotenv()
+
 # configuration de la clé d'API
 openai.api_key = os.getenv('API_KEY')
+if not openai.api_key:
+    raise ValueError("Aucune clé API n'a été trouvée. Assurez-vous de l'avoir définie dans vos variables d'environnement.")
 
 
 # création de l'instance de l'app 
@@ -34,10 +38,12 @@ def index():
 @app.route('/postData', methods=["POST"])
 def postData():
     # entrée de l'utilisateur
+    # TODO: si le message est vide
     data = request.get_json()
     userMessage = data['message']
 
-    messages = [ {'role': 'system', 'content': 'You are inteligent assistant'}]
+    # TODO: donner la possibilité à l'utilisateur de définir role et content du chatbot pour des réponses sur des thématiques spécifiques ?
+    messages = [ {'role': 'system', 'content': 'You are intelligent assistant'}]
 
     if userMessage:
         messages.append(
@@ -56,7 +62,7 @@ def postData():
     )
     # retourner le message au client (navigateur) pour l'afficher
     return { 'message': reply }
-
+    
 
 # définition de la route d'affichage de l'interface du chatbot
 @app.route('/chatbot')
@@ -69,7 +75,7 @@ def open_browser():
 
 # démarrer l'app
 if __name__ == '__main__':
-    # ouvrir le navig après 1 seconde
+    # ouvrir le navigateur après 1 seconde
     Timer(1, open_browser).start()
 
     # définir host et port si besoin
