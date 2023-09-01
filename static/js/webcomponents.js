f8c9dda0ade72e7a71c8b378a7a78931338f2a05
LSS <lss@jaimal.com>
2023-09-01 19:30:27 +0200

    test modale

--- static/js/webcomponents.js
+++ static/js/webcomponents.js
@@ -1,51 +1,61 @@
 /* COMPOSANTS HTML (WEBCOMPONENTS) */
-class MyNav extends HTMLElement {
+class MyModal extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <header>
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">
-                        <img src="/static/assets/picto_rose.png" alt="logo chatbot navbar" class="nav-logo">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <i class="fa-solid fa-ellipsis-vertical"></i>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
-                            <li class="nav-item">
-                                <!-- Bouton de la modale -->
-                                    <i class="fa-solid fa-robot" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
-                            </li>
-                            <li class="nav-item">
-                                <i class="fa-solid fa-circle-half-stroke"></i>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-        <!-- Modale centrée verticalement -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">La vie est "random" 🎲</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body" id="modalBodyJoke"></div>
-                    <div class="modal-body" id="modalBodyAdvice"></div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">OK</button>
+            <!-- Modale centrée verticalement -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">La vie est "random" 🎲</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body" id="modalBodyJoke"></div>
+                        <div class="modal-body" id="modalBodyAdvice"></div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">OK</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         `;
     }
 }
+
+
+class MyNav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <nav class="navbar navbar-expand-lg">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="/">
+                            <img src="/static/assets/picto_rose.png" alt="logo chatbot navbar" class="nav-logo">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <i class="fa-solid fa-ellipsis-vertical"></i>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
+                                <li class="nav-item">
+                                    <!-- Bouton de la modale -->
+                                        <i class="fa-solid fa-robot" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                                </li>
+                                <li class="nav-item">
+                                    <i class="fa-solid fa-circle-half-stroke"></i>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <my-modal></my-modal>
+        `;
+    }
+}
+
 
 class MyFooter extends HTMLElement {
     connectedCallback() {
@@ -68,5 +78,7 @@ class MyFooter extends HTMLElement {
     }
 }
 
+
+customElements.define('my-modal', MyModal);
 customElements.define('my-nav', MyNav);
 customElements.define('my-footer', MyFooter);
