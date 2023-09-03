6734fee89181a4621ce9faa2658fd561c3641286
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-09-03 23:23:18 +0200

    effet page d'accueil ok je pense que je dois definir la taille initiale

--- static/js/chatbot_effects.js
+++ static/js/chatbot_effects.js
@@ -61,5 +61,35 @@ document.addEventListener("DOMContentLoaded", function () {
   checkScrollPosition();
 });
 
+// Effet page d'accueil présentation
+const text = "Je suis un assistant intelligent conçu pour vous aider à trouver des réponses à vos questions. Je peux aider avec des informations,assister à la rédaction, et participer à des discussions sur divers sujets. Je suis capable de comprendre et fournir des réponses précises et pertinentes.";
+const splitText = text.split("");
+const containerText = document.querySelector('.lead');
+let showLetters = "";
+let counter = 0;
+let randomSpeed = 100;
+
+function typeEffect() {
+    let currentLetter = splitText[counter];
+    showLetters += currentLetter;
+    containerText.innerHTML = showLetters + '<span class="cursor"></span>';
+    counter++;
+
+    if(counter > splitText.length - 1){
+        setTimeout(() => {
+            // Attend 10 secondes avant de réinitialiser le message
+            showLetters = "";
+            counter = 0;
+            containerText.innerHTML = '<span class="cursor"></span>';
+            typeEffect();
+        }, 10000); // 10 secondes
+        return;
+    }
+
+    setTimeout(typeEffect, randomSpeed);
+}
+
+typeEffect();
+
 
 
