7a09537a11d35dffe7f91501737cad4243b5b809
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-09-03 14:55:44 +0200

    j'ai changé mon container en container general

--- static/js/chatbot_effects.js
+++ static/js/chatbot_effects.js
@@ -1,79 +1,84 @@
+function displayMessageWordByWord(message, containerElem) {
+    const words = message.split(' ');
+    let delay = 0;
+
+    words.forEach(word => {
+        const wordElem = document.createElement("span");
+        wordElem.classList.add("word");
+        containerElem.appendChild(wordElem); 
+
+        // Divisez chaque mot en lettres
+        const letters = word.split('');
+        let letterIndex = 0;
+
+        const displayLetterInterval = setInterval(() => {
+            if (letterIndex < letters.length) {
+                wordElem.textContent += letters[letterIndex] + (letterIndex === letters.length - 1 ? " " : ""); 
+                wordElem.classList.add("visible");
+                letterIndex++;
+            } else {
+                clearInterval(displayLetterInterval);
+            }
+        }, 100);  // Affichez chaque lettre tous les 100ms
+
+        delay += (word.length + 1) * 100;  // Incrémentez le délai en fonction du nombre de lettres
+    });
+}
 document.addEventListener("DOMContentLoaded", function() {
     // Références aux éléments
-    const userInput = document.getElementById("userInput");
-    const sendButton = document.getElementById("sendButton");
+    const scrollArrow = document.getElementById("scrollArrow");
     const responseArea = document.getElementById("responseArea");
 
-    sendButton.addEventListener("click", function() {
-        const userMessage = userInput.value;
+    // Fonction pour vérifier la position de défilement
+    function checkScrollPosition() {
+        // Si l'utilisateur est près du bas de la zone de réponse (avec une tolérance de 5 pixels)
+        if (responseArea.scrollHeight - responseArea.scrollTop <= responseArea.clientHeight + 5) {
+            scrollArrow.classList.add("hidden"); // Cache la flèche
+        } else {
+            scrollArrow.classList.remove("hidden"); // Affiche la flèche
+        }
+    }
 
-        const userMessageElem = document.createElement("div");
-        userMessageElem.textContent = userMessage;
-        userMessageElem.classList.add("message", "user-message");
-        responseArea.appendChild(userMessageElem);
+    // Lorsque l'utilisateur fait défiler la zone de réponse, vérifiez sa position
+    responseArea.addEventListener("scroll", checkScrollPosition);
 
-        userInput.value = "";
-
-        fetch('/postData', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ message: userMessage })
-        })
-        .then(response => response.json())
-        .then(data => {
-            const botMessageElem = document.createElement("div");
-            botMessageElem.classList.add("message", "bot-message");
-            responseArea.appendChild(botMessageElem);
-            
-            displayMessageWordByWord(data.message, botMessageElem);
-        })
-        .catch(error => {
-            console.error('Erreur :', error);
-        });
+    // Lorsque l'utilisateur clique sur la flèche, faites défiler la zone de réponse vers le bas
+    scrollArrow.addEventListener("click", function() {
+        responseArea.scrollTop = responseArea.scrollHeight;
     });
 
-    function displayMessageWordByWord(message, containerElem) {
-        const words = message.split(' ');
-        let delay = 0;
-
-        words.forEach(word => {
-            setTimeout(() => {
-                const wordElem = document.createElement("span");
-                wordElem.textContent = word + " ";
-                wordElem.classList.add("word");
-                containerElem.appendChild(wordElem); // Change to append to containerElem
-
-                setTimeout(() => {
-                    wordElem.classList.add("visible");
-                }, 50);
-            }, delay);
-            
-            delay += 300;
-        });
-    }
+    // Au chargement, vérifiez la position de défilement initiale
+    checkScrollPosition();
 });
 
+// Création de l'élément du message du bot
+const botMessageElem = document.createElement("div");
+botMessageElem.classList.add("message", "bot-message");
 
+// Création du bouton
+const copyBtn = document.createElement("button");
+copyBtn.innerHTML = 'Copier';
+copyBtn.classList.add("copy-btn");
+botMessageElem.appendChild(copyBtn); // Ajout du bouton à l'élément du message
 
+// Ajout de l'écouteur d'événements au bouton
+copyBtn.addEventListener("click", function(e) {
+    e.stopPropagation(); // Empêchez tout autre événement
 
-const text = "Voici mon portfolio illustré, mes projets sont réalisés en HTML, CSS, JS, PHP, SQL, et Python"
-const splitText = text.split("")
-const containerText = document.querySelector('p')
-let showLetters = ""
-let counter = 0
-let randomSpeed = 100
+    navigator.clipboard.writeText(data.message)
+        .then(function() {
+            console.log('Text successfully copied to clipboard!');
+        })
+        .catch(function(err) {
+            console.error('Could not copy text: ', err);
+        });
+});
 
-setInterval(function(){
-    let curentLetter = splitText[counter]
-    showLetters += curentLetter
-    containerText.innerText = showLetters
-    counter++
+// Création de l'élément de texte pour le message
+const messageTextElem = document.createElement("span");
+messageTextElem.textContent = data.message;
+botMessageElem.appendChild(messageTextElem); // Ajout du texte à l'élément du message
 
-    if(counter > splitText.length - 1){
-        counter = 0
-        showLetters = ""
-    }
-}, randomSpeed)
+// Ajout de l'élément complet du message à la zone de réponse
+responseArea.appendChild(botMessageElem);
 
