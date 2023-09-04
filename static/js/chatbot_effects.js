2d81934b428c7064d57c07e75f20da8fc6c12b4d
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-09-04 19:38:48 +0200

    message d'accueil plus rapide et stop une fois entier

--- static/js/chatbot_effects.js
+++ static/js/chatbot_effects.js
@@ -62,34 +62,27 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Effet page d'accueil présentation
-const text = "Je suis un assistant intelligent conçu pour vous aider à trouver des réponses à vos questions. Je peux aider avec des informations,assister à la rédaction, et participer à des discussions sur divers sujets. Je suis capable de comprendre et fournir des réponses précises et pertinentes.";
+const text =
+  "Je suis un assistant intelligent conçu pour vous aider à trouver des réponses à vos questions. Je peux aider avec des informations, assister à la rédaction, et participer à des discussions sur divers sujets. Je suis capable de comprendre et fournir des réponses précises et pertinentes.";
 const splitText = text.split("");
-const containerText = document.querySelector('.lead');
+const containerText = document.querySelector(".lead");
 let showLetters = "";
 let counter = 0;
-let randomSpeed = 100;
+//RandomSpeed a 50 au lieu de 100 (plus rapide)
+let randomSpeed = 50;
 
 function typeEffect() {
-    let currentLetter = splitText[counter];
-    showLetters += currentLetter;
-    containerText.innerHTML = showLetters + '<span class="cursor"></span>';
-    counter++;
+  let currentLetter = splitText[counter];
+  showLetters += currentLetter;
+  containerText.innerHTML = showLetters + '<span class="cursor"></span>';
+  counter++;
 
-    if(counter > splitText.length - 1){
-        setTimeout(() => {
-            // Attend 10 secondes avant de réinitialiser le message
-            showLetters = "";
-            counter = 0;
-            containerText.innerHTML = '<span class="cursor"></span>';
-            typeEffect();
-        }, 10000); // 10 secondes
-        return;
-    }
+  if (counter > splitText.length - 1) {
+    // Si le message est entièrement affiché, arrêtez la fonction
+    return;
+  }
 
-    setTimeout(typeEffect, randomSpeed);
+  setTimeout(typeEffect, randomSpeed);
 }
 
 typeEffect();
-
-
-
